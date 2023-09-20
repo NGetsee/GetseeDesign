1abcf2c1dc142ec41d152915baf3b62736429b05
NGetsee <123643158+NGetsee@users.noreply.github.com>
2023-09-20 11:24:38 +0100

    include db

--- assets/php/form-process_old.php
+++ assets/php/form-process_old.php
@@ -1,6 +1,23 @@
 <?php
 
-$errorMSG = "";
+
+// Replace with your actual MySQL connection details
+$host = "localhost";
+$username = "root";
+$password = "root";
+$database = "getsee_design";
+
+// Create a MySQL connection
+$conn = new mysqli($host, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+;
+
+
 
 // NAME
 if (empty($_POST["name"])) {
@@ -73,4 +90,20 @@ if ($success && $errorMSG == ""){
     }
 }
 
+// Retrieve form data
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+
+// Insert data into MySQL database
+$sql = "INSERT INTO contacts (name, email, message) VALUES ('$name', '$email', '$message')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Form data has been successfully submitted.";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close the MySQL connection
+$conn->close()
 ?>
